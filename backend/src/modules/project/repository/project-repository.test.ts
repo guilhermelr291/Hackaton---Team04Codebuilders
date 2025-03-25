35403a7e6e4ce30e4a238f866a7f48287d33ce42
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-25 20:10:28 -0300

    test: ensure ProjectRepository.create return created project

--- backend/src/modules/project/repository/project-repository.test.ts
+++ backend/src/modules/project/repository/project-repository.test.ts
@@ -1,14 +1,39 @@
-import { test, describe, beforeEach, vi, expect } from 'vitest';
+import {
+  test,
+  describe,
+  beforeEach,
+  vi,
+  expect,
+  beforeAll,
+  afterAll,
+} from 'vitest';
 import prisma from '../../../prisma/db';
 import { CreateProjectParams } from '../service/project-service';
 import { ProjectRepository } from './project-repository';
 
 vi.mock('../../../prisma/db', () => ({
   default: {
-    project: { create: vi.fn() },
+    project: {
+      create: vi.fn().mockResolvedValue({
+        id: 1,
+        name: 'any_name',
+        clientId: 1,
+        userId: 1,
+        status: 'IN_PROGRESS',
+        price: 10,
+      }),
+    },
   },
 }));
 
+const mockProjectModel = () => ({
+  id: 1,
+  name: 'any_name',
+  clientId: 1,
+  userId: 1,
+  status: 'IN_PROGRESS',
+  price: 10,
+});
 const mockProjectParams = (): CreateProjectParams => ({
   name: 'any_name',
   clientId: 1,
@@ -35,6 +60,11 @@ describe('ProjectRepository', () => {
       expect(prisma.project.create).toHaveBeenCalledWith({
         data: createProjectParams,
       });
+    });
+    test('Should return created project', async () => {
+      const result = await sut.create(mockProjectParams());
+
+      expect(result).toStrictEqual(mockProjectModel());
     });
   });
 });
