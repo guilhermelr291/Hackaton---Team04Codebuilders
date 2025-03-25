674c09add15cf77ec8570d350c8727062a1544c2
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-25 19:51:26 -0300

    feat: create project service

---
+++ backend/src/modules/project/service/project-service.ts
@@ -0,0 +1,33 @@
+import { UnprocessableEntity } from '../../../common/errors/http-errors';
+import { ClientRepository } from '../../client/repository/client-repository';
+import { ProjectRepository } from '../repository/project-repository';
+
+export type CreateProjectParams = {
+  name: string;
+  clientId: number;
+  userId: number;
+  status?:
+    | 'PLANNING'
+    | 'IN_PROGRESS'
+    | 'COMPLETED'
+    | 'PENDING_PAYMENT'
+    | 'OVERDUE';
+  price?: number;
+};
+
+export class ProjectService {
+  constructor(
+    private readonly projectRepository: ProjectRepository,
+    private readonly clientRepository: ClientRepository
+  ) {
+    this.projectRepository = projectRepository;
+    this.clientRepository = clientRepository;
+  }
+
+  async create(data: CreateProjectParams) {
+    const client = await this.clientRepository.getById(data.clientId);
+    if (!client) throw new UnprocessableEntity('Cliente não encontrado');
+
+    await this.projectRepository.create(data);
+  }
+}
