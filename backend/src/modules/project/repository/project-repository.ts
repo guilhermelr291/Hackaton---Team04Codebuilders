4273f20105a2327aad670dbc16077a29e7240d7d
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-25 19:51:39 -0300

    feat: create project repository

---
+++ backend/src/modules/project/repository/project-repository.ts
@@ -0,0 +1,8 @@
+import prisma from '../../../prisma/db';
+import { CreateProjectParams } from '../service/project-service';
+
+export class ProjectRepository {
+  async create(data: CreateProjectParams) {
+    await prisma.project.create({ data });
+  }
+}
