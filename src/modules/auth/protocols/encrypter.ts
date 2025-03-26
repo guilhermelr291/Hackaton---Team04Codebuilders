fdc4d4652b50bd9e5fc58e4118df83cc3354c24a
Breno Silva <brenosill@hotmail.com>
2025-03-26 13:59:04 -0300

    :wastebasket: remove: Arquivos da main

--- src/modules/auth/protocols/encrypter.ts
+++
@@ -1,3 +0,0 @@
-export interface Encrypter {
-  encrypt(data: {}): string;
-}
--- src/server.ts
+++
@@ -1,5 +0,0 @@
-import app from './config/app';
-import dotenv from 'dotenv';
-dotenv.config();
-
-app.listen(5050, () => console.log('Server running at: http://localhost:5050'));
