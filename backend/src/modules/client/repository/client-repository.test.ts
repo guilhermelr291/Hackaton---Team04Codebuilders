e823085d4b0bdc4853c2b3d0c00832b994e24565
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-25 21:25:26 -0300

    test: ensure ClientRepository.getById calls prisma with correct value

---
+++ backend/src/modules/client/repository/client-repository.test.ts
@@ -0,0 +1,35 @@
+import { vi, test, describe, expect, beforeEach } from 'vitest';
+
+import { ClientRepository } from './client-repository';
+import prisma from '../../../prisma/db';
+
+vi.mock('../../../prisma/db', () => ({
+  default: {
+    client: {
+      findUnique: vi.fn().mockResolvedValue({
+        id: 1,
+        name: 'any_name',
+        email: 'any_email@mail.com',
+      }),
+    },
+  },
+}));
+
+describe('ClientRepository', () => {
+  let clientRepository: ClientRepository;
+
+  beforeEach(() => {
+    clientRepository = new ClientRepository();
+    // Clear all mocks before each test
+    vi.clearAllMocks();
+  });
+
+  describe('getById', () => {
+    test('Should call prisma.client.findUnique with correct value', async () => {
+      const id = 1;
+      await clientRepository.getById(id);
+
+      expect(prisma.client.findUnique).toHaveBeenCalledWith({ where: { id } });
+    });
+  });
+});
