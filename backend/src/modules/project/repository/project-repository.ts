16b0d54e5e66656c784e91ea982381b1a968edd5
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-27 16:42:36 -0300

    feat: create update method in ProjectRepository

--- backend/src/modules/project/repository/project-repository.ts
+++ backend/src/modules/project/repository/project-repository.ts
@@ -1,9 +1,16 @@
 import prisma from '../../../prisma/db';
-import { CreateProjectParams } from '../service/project-service';
+import {
+  CreateProjectParams,
+  updateProjectParams,
+} from '../service/project-service';
 
 export class ProjectRepository {
   async create(data: CreateProjectParams) {
     const project = await prisma.project.create({ data });
+    return project;
+  }
+  async update(id: number, data: updateProjectParams) {
+    const project = await prisma.project.update({ where: { id }, data });
     return project;
   }
 }
