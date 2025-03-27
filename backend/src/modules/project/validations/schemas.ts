6f689b4fcfc1138bdf5e9d5744581efca723ef8a
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-27 17:41:36 -0300

    fix: fix clientId validation in CreateProjectSchema

--- backend/src/modules/project/validations/schemas.ts
+++ backend/src/modules/project/validations/schemas.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const CreateProjectSchema = z.object({
   name: z.string(),
-  clientId: z.coerce.number(),
+  clientId: z.coerce.number().positive(),
   status: z
     .enum([
       'PLANNING',
