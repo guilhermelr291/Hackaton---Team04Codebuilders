908d54273c68d54de16341f178076643e55b7418
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-28 19:31:49 -0300

    feat: add update and delete methods to timeEntryService

--- backend/src/modules/time-entry/service/time-entry-service.ts
+++ backend/src/modules/time-entry/service/time-entry-service.ts
@@ -1,10 +1,20 @@
-import { UnprocessableEntity } from '../../../common/errors/http-errors';
+import {
+  NotFound,
+  UnprocessableEntity,
+} from '../../../common/errors/http-errors';
 import { ProjectRepository } from '../../project/repository/project-repository';
 import { TimeEntryRepository } from '../repository/time-entry-repository';
 
 export type CreateTimeEntryParams = {
   userId: number;
   projectId: number;
+  duration: number;
+  description: string;
+  date: Date;
+};
+export type UpdateTimeEntryParams = {
+  id: number;
+  userId: number;
   duration: number;
   description: string;
   date: Date;
@@ -27,5 +37,22 @@ export class TimeEntryService {
     if (!project) throw new UnprocessableEntity('Projeto não encontrado');
 
     await this.timeEntryRepository.create(data);
+  }
+
+  async update(data: UpdateTimeEntryParams) {
+    const timeEntry = await this.timeEntryRepository.getById(
+      data.id,
+      data.userId
+    );
+    if (!timeEntry) throw new NotFound('TimeEntry não encontrado');
+
+    await this.timeEntryRepository.update(data);
+  }
+
+  async delete(id: number, userId: number) {
+    const timeEntry = await this.timeEntryRepository.getById(id, userId);
+    if (!timeEntry) throw new NotFound('TimeEntry não encontrado');
+
+    await this.timeEntryRepository.delete(id, userId);
   }
 }
