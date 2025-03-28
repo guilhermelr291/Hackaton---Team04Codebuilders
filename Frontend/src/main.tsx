98f5d6fc93cba46869c18e5564ef58c9f1b633a8
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-03-28 10:51:01 -0300

    🚚 feat: Indentação do código

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,3 +1,4 @@
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/css/index.css'
@@ -5,9 +6,9 @@ import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App /> 
-    </BrowserRouter>
-  </StrictMode>,
+	<StrictMode>
+		<BrowserRouter>		
+			<App />
+		</BrowserRouter>
+	</StrictMode>
 )
