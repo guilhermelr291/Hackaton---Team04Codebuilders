8d2f551ee8af0c2e18b6ecbe85882a3e88536209
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-27 22:09:28 -0300

    feat: add create and update methods to ClientRepository

--- backend/src/modules/client/repository/client-repository.ts
+++ backend/src/modules/client/repository/client-repository.ts
@@ -1,7 +1,17 @@
 import prisma from '../../../prisma/db';
+import { ClientParams } from '../service/client-service';
 
 export class ClientRepository {
   async getById(id: number) {
     return await prisma.client.findUnique({ where: { id } });
+  }
+
+  async create(data: ClientParams) {
+    const client = await prisma.client.create({ data });
+    return client;
+  }
+  async update(id: number, data: ClientParams) {
+    const client = await prisma.client.update({ where: { id }, data });
+    return client;
   }
 }
