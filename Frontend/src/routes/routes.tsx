4cec48d27d76bb5e9e22235d39c66de9455641c9
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-03-28 11:28:45 -0300

    ✨ feat: Adicionado novas rotas e layout

--- Frontend/src/routes/routes.tsx
+++ Frontend/src/routes/routes.tsx
@@ -5,12 +5,19 @@ import { Loader } from "lucide-react";
 import { Home } from "../pages/Home";
 import  SignIn  from "../pages/auth/signin";
 import  SignUp from "../pages/auth/signup";
+import DashboardLayout  from "../components/Layout/DashboardLayout";
+import  Dashboard  from "../pages/dashboard/DashboardHome";
+import  Clientes  from "../pages/dashboard/Clientes";
+import  Projetos  from "../pages/dashboard/Projetos";
+import  Tempo  from "../pages/dashboard/Tempo";
+import  Faturas  from "../pages/dashboard/Faturas";
+import  Relatorios  from "../pages/dashboard/Relatorios";
+
 
 
 const RoutesComponent = () => {
 
     const {authuser,CheckAuth,isCheckAuth} =  useAuthStore()
-
     useEffect(() => {
         CheckAuth()
       },[CheckAuth])
@@ -26,11 +33,20 @@ const RoutesComponent = () => {
   return (
     <div>
         <Routes >
-          <Route path="/" element={ <Home /> } /> 
-          <Route path="/signup" element={!authuser ? <SignUp  /> : <Navigate to={'/'} />} />
-          <Route path="/signin" element={!authuser ?  <SignIn/> : <Navigate to={'/'} />} />
-          {/* <Route path="/settings" element={<SettingsPage />}/>
-          <Route path="/profile" element={authuser ? <Profile /> : <Navigate to={'/login'}/> }/> */}
+
+          <Route path="/" element={!authuser ?  <Home /> : <Navigate to={'/dashboard'} />}/> 
+          <Route path="/signup" element={!authuser ? <SignUp  /> : <Navigate to={'/dashboard'} />} />
+          <Route path="/signin" element={!authuser ?  <SignIn/> : <Navigate to={'/dashboard'} />} />
+         
+          <Route path="/dashboard" element={authuser ?  <DashboardLayout/> : <Navigate to={'/'} />}>
+              <Route index element={<Dashboard />} />
+              <Route path="clientes" element={<Clientes />} />
+              <Route path="projetos" element={<Projetos />} />
+              <Route path="tempo" element={<Tempo />} />
+              <Route path="faturas" element={<Faturas />} />
+              <Route path="relatorios" element={<Relatorios />} />
+            </Route>
+
 
         </Routes>
       
