4d8b2ec355d37d30dab53a2ac23630125fc9a66d
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-27 22:09:38 -0300

    feat: add create and update methods to ClientService

--- backend/src/modules/client/service/client-service.ts
+++ backend/src/modules/client/service/client-service.ts
@@ -1,6 +1,16 @@
 import { NotFound } from '../../../common/errors/http-errors';
 import { ClientRepository } from '../repository/client-repository';
 
+export type ClientParams = {
+  name: string;
+  email: string;
+  phone?: string;
+  address?: string;
+  city?: string;
+  neighborhood?: string;
+  postalCode?: string;
+};
+
 export class ClientService {
   constructor(private readonly clientRepository: ClientRepository) {
     this.clientRepository = clientRepository;
@@ -10,5 +20,15 @@ export class ClientService {
     const client = await this.clientRepository.getById(id);
     if (!client) throw new NotFound('Cliente não encontrado');
     return client;
+  }
+  async create(data: ClientParams) {
+    await this.clientRepository.create(data);
+  }
+
+  async update(id: number, data: ClientParams) {
+    const client = await this.clientRepository.getById(id);
+    if (!client) throw new NotFound('Cliente não encontrado');
+
+    await this.clientRepository.update(id, data);
   }
 }
