1ac37d9786eadc6b5befd7ab805f310a05e5e531
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-28 19:31:40 -0300

    feat: add update and delete methods to timeEntryRepository

--- backend/src/modules/time-entry/repository/time-entry-repository.ts
+++ backend/src/modules/time-entry/repository/time-entry-repository.ts
@@ -1,9 +1,29 @@
 import prisma from '../../../prisma/db';
-import { CreateTimeEntryParams } from '../service/time-entry-service';
+import {
+  CreateTimeEntryParams,
+  UpdateTimeEntryParams,
+} from '../service/time-entry-service';
 
 export class TimeEntryRepository {
+  async getById(id: number, userId: number) {
+    const timeEntry = await prisma.timeEntry.findUnique({
+      where: { id, userId },
+    });
+    return timeEntry;
+  }
   async create(data: CreateTimeEntryParams) {
     const timeEntry = await prisma.timeEntry.create({ data });
     return timeEntry;
+  }
+  async update(data: UpdateTimeEntryParams) {
+    const { id, userId, ...timeEntryData } = data;
+    const timeEntry = await prisma.timeEntry.update({
+      where: { id, userId },
+      data: timeEntryData,
+    });
+    return timeEntry;
+  }
+  async delete(id: number, userId: number) {
+    await prisma.timeEntry.delete({ where: { id, userId } });
   }
 }
