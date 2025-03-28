f422da684ccf11d78f57b3c9344faa610d583486
Breno Silva <109807534+brenosilldev@users.noreply.github.com>
2025-03-28 10:48:02 -0300

    :lipstick: feat: Alterado estilos do header e adicionado button

--- Frontend/src/components/Header/Header.tsx
+++ Frontend/src/components/Header/Header.tsx
@@ -1,22 +1,27 @@
-import { Menu } from "lucide-react";
+import { Menu ,LogOut } from "lucide-react";
 import { useAuthStore } from "../../store/useAuthStore";
+// import {Button} from "@heroui/react";
+import { Button } from "@/components/ui/button"
+
 
 type HeaderProps = {
     setSidebarOpen: (open: boolean) => void;
-  };
+};
   
-  function Header({ setSidebarOpen }: HeaderProps) {
+const Header = ({ setSidebarOpen }: HeaderProps)=> {
 
     const { logout,authuser } = useAuthStore();
 
     return (
-		<header className="bg-[#FFFFFF] border-b border-solid border-b-blue-100/500  text-blue-900 shadow-md p-3 flex items-center justify-between">
+		<header className="w-full bg-[#FFFFFF] border-b border-solid border-b-blue-100/500  text-blue-900 shadow-md py-2 px-1  md:px-4 md:py-4 flex items-center justify-between">
 			<button className="md:hidden" onClick={() => setSidebarOpen(true)}>
 				<Menu size={24} />
 			</button>
 
 			<h1 className="text-xl font-bold">{authuser?.name}</h1>
-			<button className="cursor-pointer btn btn-sm w-14 p-2 rounded-md text-white font-bold bg-black" onClick={logout}>Sair </button>
+
+      		<Button color="secondary" onClick={logout} >Sair <LogOut className=" h-4 w-4" /> </Button>
+		
 		</header>
     );
   }
