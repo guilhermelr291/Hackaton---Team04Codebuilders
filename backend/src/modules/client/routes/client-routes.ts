98b3956b189b81c5651720d7b88683aa57a25801
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-27 22:09:58 -0300

    feat: add create and update client routes

--- backend/src/modules/client/routes/client-routes.ts
+++ backend/src/modules/client/routes/client-routes.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import { makeClientController } from '../../../common/factories/auth/client-controller-factory';
+import { validate } from '../../../common/middlewares/validation-middleware';
+import { ClientSchema } from '../validations/schemas';
 
 const clientController = makeClientController();
 
 export default (router: Router): void => {
   router.get('/clients/:id', (req, res, next) =>
     clientController.getById(req, res, next)
+  );
+  router.post('/clients', validate(ClientSchema), (req, res, next) =>
+    clientController.create(req, res, next)
+  );
+  router.patch('/clients/:id', validate(ClientSchema), (req, res, next) =>
+    clientController.update(req, res, next)
   );
 };
