5ff9f223a14153aaafa45125895d6229486a890f
guilhermelr291 <guilhermelr291dev@gmail.com>
2025-03-28 21:14:56 -0300

    refactor: change api port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,4 +2,7 @@ import app from './config/app';
 import dotenv from 'dotenv';
 dotenv.config();
 
-app.listen(5050, () => console.log('Server running at: http://localhost:5050'));
+const port = Number(process.env.PORT) || 5050;
+app.listen(port, () =>
+  console.log(`Server running at: http://localhost:${port}`)
+);
